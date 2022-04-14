e932626a75eaf512d348716b3b33d5e0f85565ba
shartzelUNCC <71344692+shartzelUNCC@users.noreply.github.com>
2022-04-14 16:38:57 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/mapbox/streets-v11',
+  accessToken:
+    'pk.eyJ1Ijoic2FtbXktYiIsImEiOiJjbDF5ZHB3aGQwNjdnM2NvNncxZzB0d2ZyIn0.nkbEjS8P_tgz3ZW3YrHuMA',
+  CSV: './DisCourse_Dataset.csv',
+  center: [35.23165, -80.84275],
+  zoom: 6,
+  title: 'Choose from a list of filters to select the best course for you!',
+  description:
+    'You can select from different options to find the course for you based on terrain type, difficulty, distance, and duration to play.',
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
+  popupInfo: ['Location_Name'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Terrain: ',
+      columnHeader: 'Terrain',
+      listItems: [
+        'Flat',
+        'Hilly',
+        'Mixed',
+      ],
+    },
+    {
+      type: 'dropdown',
+      title: 'Difficulty: ',
+      columnHeader: 'Difficulty',
+      listItems: [
+        'Beginner',
+        'Amateur',
+        'Intermediate',
+        'Advanced',
+        'Pro',
+      ],
+    },
+    {
+      type: 'dropdown',
+      title: 'Distance: ',
+      columnHeader: 'Distance',
+      listItems: [
+        'Short',
+        'Medium',
+        'Long',
+      ],
+    },
+    {
+      type: 'dropdown',
+      title: 'Duration to Play: ',
+      columnHeader: 'Duration',
+      listItems: [
+        '1',
+        '1.5',
+        '2',
+        '2.5',
+      ],
+    },
+  ],
+};
